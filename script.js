d68bbd90c2f6bf9709a3fe596f968c3cfcac153b
catfish999 <fannyli1224@gmail.com>
2025-02-18 05:14:46 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,78 @@
+
+function changefbnoodle(amount) {
+  let input = document.getElementById("fbnoodle");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
+function changefpnoodle(amount) {
+  let input = document.getElementById("fpnoodle");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
+function changefnoodle(amount) {
+  let input = document.getElementById("fnoodle");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
+function changebcookie(amount) {
+  let input = document.getElementById("bcookie");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
+function changepcookie(amount) {
+  let input = document.getElementById("pcookie");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
+function changebcake(amount) {
+  let input = document.getElementById("bcake");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
+function changepcake(amount) {
+  let input = document.getElementById("pcake");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
+function changepcrice(amount) {
+  let input = document.getElementById("pcrice");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
+function changebrice(amount) {
+  let input = document.getElementById("brice");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
+function changeprice(amount) {
+  let input = document.getElementById("price");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
+function changeporkchop(amount) {
+  let input = document.getElementById("porkchop");
+  let origin = parseInt(input.value);
+  let newValue = origin + amount;
+  if (newValue < 0) newValue = 0;
+  input.value = newValue;
+}
